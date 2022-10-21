9b8a6a452189bef4195b2e36ba64570cd6120781
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-10-21 17:22:55 +0300

    fix: user can add only date with day/month/year format

--- app/Http/Requests/Api/AuthUser/RegisterRequest.php
+++ app/Http/Requests/Api/AuthUser/RegisterRequest.php
@@ -65,7 +65,7 @@ class RegisterRequest extends FormRequest
             'password' => 'required|string|min:6|max:30',
             'bio' => 'string|max:255',
             'nick_name' => 'string|max:255',
-            'date_of_birth' => 'required|string|max:255',
+            'date_of_birth' => 'required|date_format:d-m-Y|max:255',
             'phone' => 'required|min:6|max:15|unique:users,phone',
             'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048'
         ];
@@ -91,8 +91,8 @@ class RegisterRequest extends FormRequest
             'nick_name.string' => __('messages.authUser.nick_name.string'),
             'nick_name.max' => __('messages.authUser.nick_name.max'),
             'date_of_birth.required' => __('messages.authUser.date_of_birth.required'),
-            'date_of_birth.string' => __('messages.authUser.date_of_birth.string'),
-            'date_of_birth.max' => __('messages.authUser.date_of_birth.max'),
+            'date_of_birth.date_format' => __('messages.user.date_of_birth.date_format'),
+            'date_of_birth.max' => __('messages.user.date_of_birth.max'),
             'phone.required' => __('messages.authUser.phone.required'),
             'phone.min' => __('messages.authUser.phone.min'),
             'phone.unique' => __('messages.authUser.phone.unique'),
