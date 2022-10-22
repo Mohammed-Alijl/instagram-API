7e223f0a9caa5555ad6262db9aeea2c5526574ce
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-10-22 10:59:22 +0300

    edit: change url to localhost temporarily for the test

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,7 @@
 return [
     'POST_PAGINATION' => 10,
     'COMMENT_PAGINATION' => 10,
-    'WEBSITE_URL'=>'https://instagram.devyzer.com/',
+//    'WEBSITE_URL'=>'https://instagram.devyzer.com/',
+    'WEBSITE_URL'=>'http://localhost/laravel9/instagram/public/api',
     'FOLLOW_PAGINATION'=>20
 ];
