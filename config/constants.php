a25e99c7bd99e5aceb2dad491e593743a8e2fb33
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-10-24 15:25:41 +0300

    edit: edit url of system to new host

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,6 @@ return [
     'COMMENT_PAGINATION' => 10,
     'FOLLOW_PAGINATION' => 20,
     'USER_PAGINATION' => 10,
-//    'WEBSITE_URL'=>'https://instagram.devyzer.com/',
-    'WEBSITE_URL' => 'http://localhost/laravel9/instagram/',
+    'WEBSITE_URL'=>'https://instagram-clone.devyzer.com/',
+//    'WEBSITE_URL' => 'http://localhost/laravel9/instagram/',
 ];
