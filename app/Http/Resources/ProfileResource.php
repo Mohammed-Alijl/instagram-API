92574227b1af2c2c3d54d16815f98efc77f099d4
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-10-25 13:10:46 +0300

    edit: return if auth user follow the user in profile api

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -3,26 +3,40 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class ProfileResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
+        if (auth('user')->id() == $this->id)
+            return [
+                'user_id' => $this->id,
+                'name' => $this->name,
+                'nick_name' => $this->nick_name,
+                'bio' => $this->bio,
+                'image_url' => config('constants.WEBSITE_URL') . '/public/img/users/profile/' . $this->image,
+                'posts_num' => $this->posts()->count(),
+                'followers_num' => $this->followers()->count(),
+                'following_num' => $this->follow()->count(),
+            ];
+        $followHim = DB::table('user_followers')->where(['user_id' => $this->id, 'follower_id' => auth('user')->id()])->first();
         return [
-            'user_id'=>$this->id,
-            'name'=>$this->name,
-            'nick_name'=>$this->nick_name,
-            'bio'=>$this->bio,
-            'image_url'=>config('constants.WEBSITE_URL') . '/public/img/users/profile/' . $this->image,
-            'posts_num'=>$this->posts()->count(),
-            'followers_num'=>$this->followers()->count(),
-            'following_num'=>$this->follow()->count()
+            'user_id' => $this->id,
+            'name' => $this->name,
+            'nick_name' => $this->nick_name,
+            'bio' => $this->bio,
+            'image_url' => config('constants.WEBSITE_URL') . '/public/img/users/profile/' . $this->image,
+            'posts_num' => $this->posts()->count(),
+            'followers_num' => $this->followers()->count(),
+            'following_num' => $this->follow()->count(),
+            'youFollowHim' => (bool)$followHim,
         ];
     }
 }
