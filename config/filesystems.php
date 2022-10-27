b500506227d7596b1305945482b7acf4f4dfa7a9
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-10-27 18:58:18 +0300

    fix: store profile image

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,9 +39,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
+        ],
+
+        'profile' => [
+            'driver' => 'local',
+            'root' => base_path() . 'public/img/users/profile/',
+            'url' => env('APP_URL') . '/public',
+            'visibility' => 'public',
         ],
 
         's3' => [
