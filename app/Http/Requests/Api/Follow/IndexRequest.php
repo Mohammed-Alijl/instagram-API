b638afeb6b27caae5b98fc21fdcf478c4b6879a8
Mohammed-Alajel <moh.wa.2016@gmail.com>
2022-11-03 17:38:34 +0200

    fix: return user resource in get all users you follow

--- app/Http/Requests/Api/Follow/IndexRequest.php
+++ app/Http/Requests/Api/Follow/IndexRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\Follow;
 
 use App\Http\Controllers\Api\Traits\Api_Response;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Exception;
 use Illuminate\Foundation\Http\FormRequest;
@@ -26,7 +27,7 @@ class IndexRequest extends FormRequest
             $user = User::find($id);
             if(!$user)
                 return $this->apiResponse(null,404,__('messages.user.notFound'));
-            return $user->follow()->paginate(config('constants.FOLLOW_PAGINATION'));
+            return UserResource::collection($user->follow()->paginate(config('constants.FOLLOW_PAGINATION')));
         }catch (Exception $ex){
             return $this->apiResponse(null,500,$ex->getMessage());
         }
